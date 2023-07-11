aa14b46f8cfc40c2754dd7b592fe3218baae4b99
kingcatttttt <ssvyat7007gmail.com>
2023-07-11 21:33:16 +0200

    cacl

--- index.cpp
+++ index.cpp
@@ -1,19 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
-{  
-    int a;
-    cin >> a;
-    switch (a)
+{
+    
+    int num1;
+    int num2;
+
+    cout << "єто калюкулятор ведите первое число" << endl;
+    cin >> num1;
+    if (num1 != NULL )
     {
-    case 1:
-        cout << "a = 1" <<endl;
-        break; 
-    case 2:
-    cout << "a = 2" <<endl;
-    default:
-    cout << "я не знаю" <<endl;
-        break;
+        cout << "ведите второе число" <<endl;
     }
+    cin >> num2;
+    if (num1 != NULL && num2 != NULL)
+    {
+        cout << "виберите действие " << "первое число ровно "  << " "<< num1 << " " << "второе число ровно " << " " <<  num2 <<endl;
+        cout << "плюс = 1" << " " << "мінус = 2" << " " << "умножить = 3" << " " << "поделить = 4"<<endl;
 }
+        int a;
+        cin >> a;
+        switch (a)
+        {
+        case 1:
+            cout << num1 << " + " << num2 << " = " <<(num1 + num2) <<endl;
+            break;
+            case 2:
+             cout << num1 << " - " << num2 << " = " <<(num1 - num2) <<endl;
+             break;
+             case 3:
+              cout << num1 << " * " << num2 << " = " <<(num1 * num2) <<endl;
+              break;
+              case 4:
+               cout << num1 << " / " << num2 << " = " <<(num1 / num2) <<endl;
+               break;
+        default:
+        cout << "ви вели не верний запрос" <<endl;
+            break;
+        }
+    }
