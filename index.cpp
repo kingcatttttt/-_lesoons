4b557e0847aca552e93e54db8948252d84bdca7c
kingcatttttt <ssvyat7007gmail.com>
2023-07-11 23:10:50 +0200

    UPdate

--- index.cpp
+++ index.cpp
@@ -3,19 +3,28 @@ using namespace std;
 
 int main()
 {
-  int i = 0;
-  while (i <= 20)
-  {
-    if(i % 2 == 0)
+    int dia,divi;
+    int i = 0;
+    cout << "ведите  число чтоб узнать єго диапозон на четность или на ваш вариант" <<endl;
+    cin >> dia;
+    if (dia != NULL)
     {
-      cout << "число " << i << " четное" <<endl;
+        cout << "ведите число на которое будет делиться без остачи" <<endl;
+        cin >> divi;
+    }
+
+   while (i <= dia)
+  {
+    if(i % divi == 0)
+    {
+      cout << "число " << i << " делиться на " << divi << " без остачи"<<endl;
     } 
     else
     {
-      cout << "число " << i << " не четное" <<endl;
+      cout << "число " << i << " не делиться на " << divi << " без остачи" <<endl;
     }
     i++;
   }
   
-  
-}
+    
+    }   
