a5f67593fc6b47d7e9c0415be5c160e6f777cc52
kingcatttttt <ssvyat7007gmail.com>
2023-07-12 22:35:28 +0200

    update add change to type line and change a type answer

--- index.cpp
+++ index.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main()
 {
+    char a;
     int dia,divi;
     int i = 0;
+    int type;
+    char change;
     cout << "ведите  число чтоб узнать єго диапозон на четность или на ваш вариант" <<endl;
     cin >> dia;
     if (dia != NULL)
@@ -13,18 +16,53 @@ int main()
         cin >> divi;
     }
 
+ if( divi != NULL)
+ {
+  cout << "виберите режим 1 = вертикальний 2 = хоризонтальний" <<endl;
+   cin >> type;
+ }
+
+ if( type != NULL)
+ {
+  cout << "виводить числа которие не деляться цело y/n" <<endl;
+   cin >> change;
+ }
+
+ if(change == 'y')
+ {
+  a = 1;
+ }
+  switch (type)
+  {
+  case 1:
    while (i <= dia)
   {
     if(i % divi == 0)
     {
-      cout << "число " << i << " делиться на " << divi << " без остачи"<<endl;
+      cout << "число " << i << " делиться на " << divi << " без остачи;"<<endl;
     } 
-    else
+    else if( a != 1)
     {
-      cout << "число " << i << " не делиться на " << divi << " без остачи" <<endl;
+      cout << "число " << i << " не делиться на " << divi << " без остачи;" <<endl;
+    }
+    i++;
+  }
+    break;
+    case 2:
+     while (i <= dia)
+  {
+    if(i % divi == 0)
+    {
+      cout << "число " << i << " делиться на " << divi << " без остачи;";
+    } 
+    else if( a != 1)
+    {
+      cout << "число " << i << " не делиться на " << divi << " без остачи;";
     }
     i++;
   }
   
-    
+  default:
+    break;
+  }
     }   
