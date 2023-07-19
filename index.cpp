80beb1dbaffb892fdb3e7f541437631e312755d6
kingcatttttt <ssvyat7007gmail.com>
2023-07-19 21:43:29 +0200

    hm

--- index.cpp
+++ index.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 int main()
 {
+ int arr[] = {0,1,3,6,84,4,68,23,7,8,-1};
+ int lenght = sizeof(arr) / sizeof(arr[0]);
+ int min = arr[0];
+    for (int i = 0; i < lenght; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+
+        
+        
+    }
+    cout << min <<endl;
     
     
 }
