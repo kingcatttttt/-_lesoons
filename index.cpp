5f2492f5c3e4ff7bd0ad45b7476360afba069ec5
kingcatttttt <ssvyat7007gmail.com>
2023-07-23 21:01:34 +0200

    sort

--- index.cpp
+++ index.cpp
@@ -3,27 +3,24 @@ using namespace std;
 
 int main()
 {
-    int arr[] = {1,2,3,3};
-    int a[4] = {};
+ int arr[] = {1,2,3,4,5,6,7,4,3,6,8};
     int lenght = sizeof(arr) / sizeof(arr[0]);
-    bool only;
-    for (int i = 0; i < lenght; i++)
+    // sort(arr,arr + lenght);
+    // for (int i = 0; i < lenght; i++)
+    // {
+    //     cout << arr[i] <<endl;
+    // }
+int number = arr[0];
+for (int i = 1; i < lenght; i++)
+{
+    if (number > arr[i])
     {
-        only = true;
-        for (int j = 0; j < i; j++)
-        {
-            if (arr[i] == arr[j])
-            {
-                only = false;
-            }
-            
-        }
-        if (only)
-        {
-            cout << arr[i] <<endl;
-        }
-        
+        number = arr[i];
     }
-   
+}
+cout << number <<endl;
+
+    
+
     
 }
