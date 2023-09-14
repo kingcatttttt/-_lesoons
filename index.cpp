52b0e98b0fbe75e9d8074ae11520cd4d39920eac
kingcatttttt <ssvyat7007gmail.com>
2023-09-14 22:46:36 +0200

    factorial

--- index.cpp
+++ index.cpp
@@ -1,54 +1,23 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
-
-
-// int main()
-// {
-//     srand(time(NULL));
-//     int const size = 10;
-//     int arr[size] = {};
-//     bool val = true;
-//     for (int i = 0; i < size; i++)
-//     {
-//         arr[i] = rand()%10;
-       
-//     }
-    
-//     for (int i = 0; i < size; i++)
-//     {
-//         val = true;
-//         for (int j = 0; j < i; j++)
-//         {
-//            if (arr[i] == arr[j])
-//            {
-//            val = false;
-//            }
-           
-//         }
-//         if (val)
-//         {
-//             cout << arr[i] << endl;
-//         }
-        
-//     }
-    
-   
-    
-// }
-
-int stack(int a)
+int fact(int N)
 {
-if (a == 10)
-{
-   return 0;
-}
-cout << a << endl;
-a++;
-return stack(a);
+    if (N == 0)
+    {
+        return 0;
+    }
+    if (N == 1)
+    {
+        return 1;
+    }
+    return N * fact(N - 1);
+    
 }
 int main()
 {
-stack(3);
+    int input;
+    cin >> input;
+    cout << fact(input) <<endl;
+
 }
