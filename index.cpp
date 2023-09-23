3b5cd498d977dc3ed858333bd12f737e3ea554c4
kingcatttttt <ssvyat7007gmail.com>
2023-09-23 21:58:41 +0200

    buble sort

--- index.cpp
+++ index.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int fact(int N)
+
+int f(int arr[], int size)
 {
-    if (N == 0)
+ for (int i = 0; i < size; i++)
     {
-        return 0;
+        if (arr[i] % 2 == 0)
+        {
+            cout << arr[i] <<endl;
+        }
+        
     }
-    if (N == 1)
-    {
-        return 1;
-    }
-    return N * fact(N - 1);
-    
 }
 int main()
 {
-    int input;
-    cin >> input;
-    cout << fact(input) <<endl;
 
+    int arr[5] = {3,4,5,1,2};
+    int const size = sizeof(arr) / sizeof(arr[0]);
+    for (int  i = 0; i < size; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                swap(arr[i],arr[j]);
+            }
+            
+            
+        }
+        
+    }
+    f(arr, 5);
+    
 }
