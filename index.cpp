25a84456494373bb83310e8bfbba9bb78ce086fa
kingcatttttt <ssvyat7007gmail.com>
2023-09-28 21:07:15 +0200

    swap()

--- index.cpp
+++ index.cpp
@@ -1,36 +1,17 @@
 #include <iostream>
 using namespace std;
+ 
 
 
-int f(int arr[], int size)
+template <typename T>
+T sw(T *a, T *b)
 {
- for (int i = 0; i < size; i++)
-    {
-        if (arr[i] % 2 == 0)
-        {
-            cout << arr[i] <<endl;
-        }
-        
-    }
+swap(*a, *b);
 }
 int main()
 {
-
-    int arr[5] = {3,4,5,1,2};
-    int const size = sizeof(arr) / sizeof(arr[0]);
-    for (int  i = 0; i < size; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (arr[i] < arr[j])
-            {
-                swap(arr[i],arr[j]);
-            }
-            
-            
-        }
-        
-    }
-    f(arr, 5);
-    
+    int a = 0, b = 1;
+cout << "a " << a << " b " << b <<endl;
+sw(&a,&b);  
+cout << "a " << a << " b " << b <<endl;
 }
